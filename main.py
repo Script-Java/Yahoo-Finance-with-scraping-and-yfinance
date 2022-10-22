2bdf396d50ec87eb596edfb536d1fc6409383f5e
Atrin Shahroudi <68357178+Script-Java@users.noreply.github.com>
2022-10-21 23:37:39 -0500

    Created basic data

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import yfinance as yf
-from list import symbol_list
 import streamlit as st
 import pprint
 
-st.write("YFinance with python")
+st.write("YFinance with python made by Atrin Shahroudi")
+st.markdown("Made with ```-Streamlit``` ```-yFinance``` ```-BeautifulSoup```")
 st.write("Choose some tickers on the sidebar to get started")
 
 with st.sidebar:
-  options = st.multiselect("Symbol Tickers", symbol_list)
+    options = st.multiselect("Symbol Tickers", symbol_list)
 
 
 def get_ticker_data(options):
-  for tickers in options:
-    ticks = yf.Ticker(tickers)
-    data = ticks.info
-    name = data["shortName"]
-    price = data["regularMarketPrice"]
-    description = data["description"]
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Name", name, "")
-    col2.metric("Price", price, "")
-    col3.metric("Description", description, "")
-    
-
+    for tickers in options:
+        ticks = yf.Ticker(tickers)
+        data = ticks.info
+        name = data["shortName"]
+        price = data["regularMarketPrice"]
+        description = data["description"]
+        link = data["coinMarketCapLink"]
+        col1, col2 = st.columns(2)
+        col1.metric("Name", name, "")
+        col2.metric("Price", price, "")
+        st.write(description)
+        st.write(link)
+        graph_data = yf.download(tickers, start="2021-11-18", end="2022-10-21")
+        history = ticks.history(period=max)
+        st.write(history)
+        st.bar_chart(graph_data)
 
 if options != []:
-  get_ticker_data(options)
+    get_ticker_data(options)
